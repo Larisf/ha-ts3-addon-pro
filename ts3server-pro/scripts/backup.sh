9e4711697d8b643b3bfc7f3b9416d2a477580c83
Lars Isferding <lars.isf@gmx.de>
2025-02-25 23:48:44 +0100

    Update backup.sh

--- ts3server-pro/scripts/backup.sh
+++ ts3server-pro/scripts/backup.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
 while true; do
-  sleep $(($BACKUP_INTERVAL * 3600))
-  
-  BACKUP_NAME="ts3_backup_$(date +%Y-%m-%d_%H-%M).tar.gz"
-  tar -czvf "/backup/$BACKUP_NAME" \
-    /config/ts3server \
-    /license/ts3server.ini \
-    /app/ts3db_mysql.ini
-  
-  echo "Backup erstellt: $BACKUP_NAME"
+    sleep 86400 # 24h
+    tar -czvf "/backup/ts3-$(date +%Y-%m-%d).tar.gz" \
+        /app/ts3server.sqlitedb \
+        /app/.ts3server_license_accepted
 done

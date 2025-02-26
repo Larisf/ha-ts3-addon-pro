d312c305919280d12b2dfaffaf66cd6b603f426f
Lars Isferding <lars.isf@gmx.de>
2025-02-26 01:52:39 +0100

    Update entrypoint.sh

--- ts3server-box86/entrypoint.sh
+++ ts3server-box86/entrypoint.sh
@@ -14,5 +14,10 @@ if [ ! -f /app/.password_set ]; then
 fi
 
 # Starte API und Server
-python3 /app/api.py &
+
+# Starte API-Server mit dem virtuellen Environment
+/opt/venv/bin/python3 /app/api.py &
+
+# Starte TS3-Server
+cd /app
 exec box64 ./ts3server_minimal_runscript.sh

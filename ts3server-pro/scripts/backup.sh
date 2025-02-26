cad638f12dfa895ebe0bedd9127a36231481c56d
Lars Isferding <lars.isf@gmx.de>
2025-02-26 01:06:06 +0100

    Delete ts3server-pro directory

--- ts3server-pro/scripts/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-    sleep 86400 # 24h
-    tar -czvf "/backup/ts3-$(date +%Y-%m-%d).tar.gz" \
-        /app/ts3server.sqlitedb \
-        /app/.ts3server_license_accepted
-done
